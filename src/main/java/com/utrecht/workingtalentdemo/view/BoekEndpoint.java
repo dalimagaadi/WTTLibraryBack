7a351debc519265429c7dad478c1c49cf30995a0
chantal1501 <chantalbon1501@gmail.com>
2021-04-01 15:34:13 +0200

    Reserveringen tonen op welkom pagina

--- src/main/java/com/utrecht/workingtalentdemo/view/BoekEndpoint.java
+++ src/main/java/com/utrecht/workingtalentdemo/view/BoekEndpoint.java
@@ -8,7 +8,6 @@ import com.utrecht.workingtalentdemo.model.Exemplaar;
 
 import com.utrecht.workingtalentdemo.model.Reservering;
 import com.utrecht.workingtalentdemo.model.User;
-import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -49,7 +48,9 @@ public class BoekEndpoint {
     @PostMapping("reserveer/{isbn}/{email}")
     public Reservering addReservering(@PathVariable String isbn, @PathVariable String email){
         User _user = us.searchUser(email);
-        Reservering _res = rs.addReservering(isbn, _user);
+        Boek _boek = bs.getBoek(isbn);
+        String titel = _boek.getTitel(); 
+        Reservering _res = rs.addReservering(isbn, _user, email, titel);
          return _res;
     }
 
@@ -76,5 +77,15 @@ public class BoekEndpoint {
     @GetMapping("searchBoekAantal/{isbn}")
     public int searchBoekAantal(@PathVariable String isbn) {
     	return bs.searchBoekAantal(isbn);
+    }
+    
+    @GetMapping("reserveringen")
+    public List<Reservering> reserveringen(){
+    	return rs.findReserveringen();
+    }
+    
+    @GetMapping("userReserveringen/{email}")
+    public List<Reservering> userReserveringen(@PathVariable String email){
+    	return rs.findUserReservering(email);
     }
 }
